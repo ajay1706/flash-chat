3ebd84c2335138ae1f3d6c5a21f1e547910537a1
ajay1706 <ajay97456@gmail.com>
2019-11-13 19:27:25 +0530

    added stream builder

--- flash_chat/lib/chat_screen.dart
+++ flash_chat/lib/chat_screen.dart
@@ -75,6 +75,37 @@ print(loggedInUser.email);
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
 
+            StreamBuilder<QuerySnapshot>(
+            stream: _firestore.collection('messages').snapshots(),
+              builder: (context, snapshot){
+
+               if(!snapshot.hasData ){
+                 return Center(
+                   child: CircularProgressIndicator(
+                     backgroundColor: Colors.lightBlueAccent,
+                   ),
+                 );
+               }
+
+                final messages = snapshot.data.documents;
+                List<Text> messageWidgets = [];
+                for(var message in messages){
+                  final messageText = message.data['text'];
+                  final messageSender = message.data['sender'];
+
+                  final messageWidget = Text('$messageText from $messageSender');
+
+                  messageWidgets.add(messageWidget);
+                // ignore: missing_return
+                }
+                return Column(
+                children: messageWidgets,
+                );
+
+
+
+              },
+        ),
 
             Container(
               decoration: kMessageContainerDecoration,
